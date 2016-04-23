fdef656d527ea78e0b9da385d16677498a69ecc4
Ashfaqur Rahman <sajib.finix@gmail.com>
2016-04-24 02:52:43 +0600

    printhex.asm updated

--- OS-Nick-Blundell/printhex.asm
+++ OS-Nick-Blundell/printhex.asm
@@ -1,43 +1,36 @@
-org 0x7c00
-
-; Hex Num to be printed
-mov dx, 0x1e2f
+; This routine will print value of dx register
+; into hex
 
 ; Hex Template Manipulation
 mov bx, HEX_TEMP
 add bx, 5
 
-MLOOP:
+HLOOP:
 	cmp byte [bx], 'x'
-	je MOUT
+	je HOUT
 	mov ax, 0x000f
 	and ax, dx
 	cmp ax, 0x09
-	jg CHAR
+	jg HCHAR
 	add ax, 0x30
 
-COMM:
+HCOMM:
 	mov byte [bx], al
 	shr dx, 4
 	sub bx, 1
-	jmp MLOOP
+	jmp HLOOP
 	
-CHAR:
+HCHAR:
 	sub ax, 0x0a
 	add ax, 0x61
-	jmp COMM
+	jmp HCOMM
 
-MOUT:
+HOUT:
 
 mov bx, HEX_TEMP
 call PRINT_STRING
-
-jmp $
 
 %include "print_string.asm"
 
 HEX_TEMP:
 	db '0x0000', 0
-
-times 510-($-$$) db 0
-dw 0xaa55
